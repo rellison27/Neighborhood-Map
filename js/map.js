fc72b915cecccb8f5e4493e3826383db940958fa
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-05 22:42:06 -0400

    establishing structure

---
+++ js/map.js
@@ -0,0 +1,16 @@
+//place  all map functionality in this file
+var map;
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.771455, lng: -84.297857},
+    zoom: 13
+  });
+  var tribeca = {lat: 40.719526, lng: -74.0089934};
+  var marker = new google.maps.Marker({
+    position: tribeca,
+    map: map,
+    title: 'First Marker!'
+  });
+}
+initMap();
