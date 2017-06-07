082483779eb4130673fc4a8c08a9865dabc6bca5
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-06 23:04:53 -0400

    fix map issue and added locations

--- js/model.js
+++ js/model.js
@@ -2,29 +2,34 @@
 var model = {
   locations: [
     {
-      place: '',
-      lat: 33.771455,
-      lng: -84.297857,
+      place: 'Iberian Pig',
+      lat: 33.774657,
+      lng: -84.295992,
+      show: true
     },
     {
-      place: '',
-      lat: 33.771455,
-      lng: -84.297857,
+      place: 'Ponce City Market',
+      lat: 33.772872,
+      lng: -84.366476,
+      show: true
     },
     {
-      place: '',
-      lat: 33.771455,
-      lng: -84.297857,
+      place: 'The Vortex',
+      lat: 33.774885,
+      lng: -84.348725,
+      show: true
     },
     {
-      place: '',
-      lat: 33.771455,
-      lng: -84.297857,
+      place: "The Fox Theatre",
+      lat: 33.774501,
+      lng: -84.385718,
+      show: true
     },
     {
-      place:'',
-      lat: 33.771455,
-      lng: -84.297857,
+      place:'Georgia Aquarium',
+      lat: 33.765297,
+      lng: -84.394902,
+      show: true
     },
   ]
 };
