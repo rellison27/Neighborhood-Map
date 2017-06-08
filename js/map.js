33db2b829d799cb5c39e53654bd402e46622d00d
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-08 17:58:16 -0400

    put in infowindow feature, show's title when marker's clicked

--- js/map.js
+++ js/map.js
@@ -4,10 +4,8 @@ function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 33.771455, lng: -84.297857},
-    zoom: 13
+    zoom: 12
     });
-  };
-
   //loop  through model
     for(var i = 0; i < locations.length; i++) {
       var position = locations[i].location;
@@ -17,9 +15,27 @@ function initMap() {
         map: map,
         position: position,
         title: name,
+        animation: google.maps.Animation.DROP
+      });
+      var infowindow = new google.maps.InfoWindow();
+      marker.addListener('click', function (){
+        populateInfoWindow(this, infowindow);
       });
     }
-
+  };
+  // populateInfoWindow
+  function populateInfoWindow(marker, infowindow) {
+    //check for already opened windows
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent ('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      //clear the window when closed
+      infowindow.addListener('closeclick', function(){
+        infowindow.setMarker(null);
+      });
+    }
+  }
 
   /*
   var tribeca = {lat: 40.719526, lng: -74.0089934};
--- js/model.js
+++ js/model.js
@@ -1,35 +1,35 @@
 // model.js
 var locations = [
     {
-    // place: 'Iberian Pig',
+    place: 'Iberian Pig',
       location : {
         lat: 33.774657,
         lng: -84.295992,
       },
     },
     {
-      // place: 'Ponce City Market',
+      place: 'Ponce City Market',
       location: {
         lat: 33.772872,
         lng: -84.366476,
       },
     },
     {
-      // place: 'The Vortex',
+      place: 'The Vortex',
       location: {
         lat: 33.774885,
         lng: -84.348725,
       },
     },
     {
-      // place: "The Fox Theatre",
+      place: "The Fox Theatre",
       location: {
         lat: 33.774501,
         lng: -84.385718,
       },
     },
     {
-      // place:'Georgia Aquarium',
+      place:'Georgia Aquarium',
       location: {
         lat: 33.765297,
         lng: -84.394902,
