fecff32699e15fe738a4a75eaefa3853807d07b0
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-13 22:18:22 -0400

    steps I need to take to complete ViewModel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-//create location Constructor and filter option
+//Define a ViewModel Constructor
+
+
+
+
+// instantiate the ViewModel and activate knockout
+// -(aka apply the bindings)
+
+
+
+
+// Define obsrevable Array in ViewModel
+// Use keyword 'this' to bind the observableArray to ViewModel
+
+
+
+
+
+
+
+
+//Apply the ko foreach binding to a <ul> elment in the view and iterate
+// over the observableArray. Note the binding context
+//(aka scope) will play an important role as well
+//Also, apply, for example, the ko text binding
+//to the <li> element 
