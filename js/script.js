3ed62a2c0f4e664fe8a54300234bb5b05adee44c
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-17 18:24:30 -0400

    starting search func  over & initialized observable array here

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
+// Initialize observableArray in ViewModel
+var myObservable = ko.observableArray(this.locations);
+
 //Define a ViewModel Constructor
 var ViewModel = function () {
   var self = this;
+  // Initialize observableArray in ViewModel
+  //this.myObservable = ko.observableArray(this.locations);
 
-  self.query = ko.observable('')
+  // Defining an observableArray for the ViewModel
+  this.locationItems = ko.observableArray(locations);
 
-  self.search = ko.computed(function (value) {
-    locations.place.removeAll();
+  self.query = ko.observable('');
 
-    for(var i in locations()) {
-      if (location()[i].place.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-        locations.location.push(location()[i]);
+  this.search = ko.computed(function (value) {
+
+    for(var i in this.locationItems) {
+      if (self.locationItems[i].place.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        ViewModel.myObservable.push(self.locationItems[i]);
       }
     }
   });
   self.clickFunction = function (location){
     google.maps.event.trigger(location.marker,'click');
-  }
+  };
 };
 //Activate KO by applying the bindings
 ko.applyBindings(new ViewModel());
 
-ViewModel.query(ViewModel.search);
 
 // instantiate the ViewModel and activate knockout
 // -(aka apply the bindings)
