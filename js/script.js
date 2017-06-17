d7cce83197e6eb5007d22b8e97f10df0a9dd4d2a
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-17 00:54:36 -0400

    trying to put search function inside of the ViewModel

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 //Define a ViewModel Constructor
 var ViewModel = function () {
   var self = this;
+  query: ko.observable('')
+  search: function (value) {
+    locations.place.removeAll();
+
+    for(var i in locations()) {
+      if (location()[i].place.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        locations.location.push(location()[i]);
+      }
+    }
+  }
   self.clickFunction = function (location){
     google.maps.event.trigger(location.marker,'click');
-    console.log(location)
   }
-}
+};
 
-
+ViewModel.query.sbscribe(viewModel.search);
 
 // instantiate the ViewModel and activate knockout
 // -(aka apply the bindings)
