4c2e56a08dacc4ddaeb642b0e0a9fba8dd061ce0
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-17 18:10:41 -0400

    took out applyBindings & changed all locations references

--- js/map.js
+++ js/map.js
@@ -5,14 +5,14 @@ function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 33.771455, lng: -84.297857},
-    zoom: 12,
+    zoom: 11,
     styles: style,
     });
   //loop  through model
-    for(var i = 0; i < locations().length; i++) {
-      var position = locations()[i].location;
-      var content = locations()[i].content;
-      var name = locations()[i].place;
+    for(var i = 0; i < locations.length; i++) {
+      var position = locations[i].location;
+      var content = locations[i].content;
+      var name = locations[i].place;
       var marker = new google.maps.Marker({
         map: map,
         position: position,
@@ -29,9 +29,9 @@ function initMap() {
       marker.addListener('click', function(){
         mouseBounce(this);
       });
-       locations()[i].marker = marker;
+       locations[i].marker = marker;
     }
-    ko.applyBindings(new ViewModel());
+    //ko.applyBindings(new ViewModel());
   };
   // populateInfoWindow
   function populateInfoWindow(marker, infowindow) {
