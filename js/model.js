12acae7c7aa4c7d50119d254cdb0140787e12037
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-21 21:26:44 -0400

    beautified

--- js/model.js
+++ js/model.js
@@ -1,48 +1,45 @@
 // Locations array with all the data for my markers
-var locations = [
-    {
-    place: 'Iberian Pig',
-      location : {
-        lat: 33.774657,
-        lng: -84.295992,
-      },
+var locations = [{
+        place: 'Iberian Pig',
+        location: {
+            lat: 33.774657,
+            lng: -84.295992,
+        },
     },
     {
-      place: 'Ponce City Market',
-      location: {
-        lat: 33.772872,
-        lng: -84.366476,
-      },
+        place: 'Ponce City Market',
+        location: {
+            lat: 33.772872,
+            lng: -84.366476,
+        },
     },
     {
-      place: 'The Vortex',
-      location: {
-        lat: 33.774885,
-        lng: -84.348725,
-      },
+        place: 'The Vortex',
+        location: {
+            lat: 33.774885,
+            lng: -84.348725,
+        },
     },
     {
-      place: "The Fox Theatre",
-      location: {
-        lat: 33.774501,
-        lng: -84.385718,
-      },
+        place: "The Fox Theatre",
+        location: {
+            lat: 33.774501,
+            lng: -84.385718,
+        },
     },
     {
-      place:'Georgia Aquarium',
-      location: {
-        lat: 33.765297,
-        lng: -84.394902,
-      },
+        place: 'Georgia Aquarium',
+        location: {
+            lat: 33.765297,
+            lng: -84.394902,
+        },
     },
-  ];
-  //this is borrowed code to style my map
-  var style = [
-    {
+];
+//this is borrowed code to style my map
+var style = [{
         "featureType": "administrative",
         "elementType": "all",
-        "stylers": [
-            {
+        "stylers": [{
                 "visibility": "on"
             },
             {
@@ -53,26 +50,21 @@ var locations = [
     {
         "featureType": "landscape",
         "elementType": "all",
-        "stylers": [
-            {
-                "color": "#f2e5d4"
-            }
-        ]
+        "stylers": [{
+            "color": "#f2e5d4"
+        }]
     },
     {
         "featureType": "poi.park",
         "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#c5dac6"
-            }
-        ]
+        "stylers": [{
+            "color": "#c5dac6"
+        }]
     },
     {
         "featureType": "poi.park",
         "elementType": "labels",
-        "stylers": [
-            {
+        "stylers": [{
                 "visibility": "on"
             },
             {
@@ -83,44 +75,35 @@ var locations = [
     {
         "featureType": "road",
         "elementType": "all",
-        "stylers": [
-            {
-                "lightness": 20
-            }
-        ]
+        "stylers": [{
+            "lightness": 20
+        }]
     },
     {
         "featureType": "road.highway",
         "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#c5c6c6"
-            }
-        ]
+        "stylers": [{
+            "color": "#c5c6c6"
+        }]
     },
     {
         "featureType": "road.arterial",
         "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#e4d7c6"
-            }
-        ]
+        "stylers": [{
+            "color": "#e4d7c6"
+        }]
     },
     {
         "featureType": "road.local",
         "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#fbfaf7"
-            }
-        ]
+        "stylers": [{
+            "color": "#fbfaf7"
+        }]
     },
     {
         "featureType": "water",
         "elementType": "all",
-        "stylers": [
-            {
+        "stylers": [{
                 "visibility": "on"
             },
             {
