30ba0458c8885c122654e7ca6e275de0f957bcf1
Jonrah <rashaunellison@rashauns-mbp.attlocal.net>
2017-06-21 21:24:47 -0400

    beautified and comments deleted

--- js/script.js
+++ js/script.js
@@ -2,61 +2,38 @@
 var myObservable = ko.observableArray(this.locations);
 
 //Define a ViewModel Constructor
-var ViewModel = function () {
-  var self = this;
+var ViewModel = function() {
+    var self = this;
 
-  // Defining an observableArray for the ViewModel from parent scope
-  this.locationItems = ko.observableArray(locations);
+    // Defining an observableArray for the ViewModel from parent scope
+    this.locationItems = ko.observableArray(locations);
 
-  //initializing sifter with empty string to prevent errors this is bound to the input
-  this.sifter = ko.observable('');
-  //  function
-  this.siftItems = ko.computed(function(){
-    var sifter = self.sifter().toLowerCase();
+    //initializing sifter with empty string to prevent errors this is bound to the input
+    this.sifter = ko.observable('');
+    //  function
+    this.siftItems = ko.computed(function() {
+        var sifter = self.sifter().toLowerCase();
 
-    if(!sifter) {
-      //loop through markers
-      for (var i=0; i < self.locationItems().length; i++) {
-        if (self.locationItems()[i].marker) {
-            self.locationItems()[i].marker.setVisible(true);
+        if (!sifter) {
+            //loop through markers
+            for (var i = 0; i < self.locationItems().length; i++) {
+                if (self.locationItems()[i].marker) {
+                    self.locationItems()[i].marker.setVisible(true);
+                }
+            }
+            return self.locationItems();
+        } else {
+            return ko.utils.arrayFilter(self.locationItems(), function(locationItems) {
+                var spot = locationItems.place.toLowerCase().indexOf(sifter) !== -1;
+                locationItems.marker.setVisible(spot);
+                return spot;
+            })
         }
-      }
-      return self.locationItems();
-    } else {
-      return ko.utils.arrayFilter(self.locationItems(), function(locationItems) {
-        var spot = locationItems.place.toLowerCase().indexOf(sifter) !== -1;
-          locationItems.marker.setVisible(spot);
-          return spot;
-      })
-    }
-  }, self);
-  // Click function that allows me to click the list and get the bounce animation
-  self.clickFunction = function (location){
-    google.maps.event.trigger(location.marker,'click');
-  };
+    }, self);
+    // Click function that allows me to click the list and get the bounce animation
+    self.clickFunction = function(location) {
+        google.maps.event.trigger(location.marker, 'click');
+    };
 };
 //Activate KO by applying the bindings
 ko.applyBindings(new ViewModel());
-
-
-// instantiate the ViewModel and activate knockout
-// -(aka apply the bindings)
-
-
-
-
-// Define obsrevable Array in ViewModel
-// Use keyword 'this' to bind the observableArray to ViewModel
-
-
-
-
-
-
-
-
-//Apply the ko foreach binding to a <ul> elment in the view and iterate
-// over the observableArray. Note the binding context
-//(aka scope) will play an important role as well
-//Also, apply, for example, the ko text binding
-//to the <li> element
